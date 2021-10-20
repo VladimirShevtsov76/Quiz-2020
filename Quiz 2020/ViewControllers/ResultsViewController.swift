4faf3522799f28a1174fe3a9f539103ab7d1a1e6
Vladimir Shevtsov <work2976@gmail.com>
2021-10-20 23:19:28 +0300

    Finished UI

---
+++ Quiz 2020/ViewControllers/ResultsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ResultsViewController.swift
+//  Quiz 2020
+//
+//  Created by Vladimir Shevtsov on 20.10.2021.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
